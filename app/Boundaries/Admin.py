f41e1a5d12787bddb4a91166ecc821a43388756a
Fangbin <fangbin0429@outlook,com>
2025-05-06 00:27:53 +0800

    clena up reduntant file and code - cleaner

--- app/Boundaries/Admin.py
+++ app/Boundaries/Admin.py
@@ -1,30 +1,7 @@
 from flask import Blueprint, request, jsonify, render_template
-from app.Controllers.Admin_related.CreateUser import CreateUserController
-from app.Controllers.Admin_related.GetAllUsers import GetAllUsersController
 from app.Boundaries.Login import login_required
 
 admin_bp = Blueprint('admin', __name__)
-
-@admin_bp.route('/create-user', methods=['POST'])
-@login_required
-def createUser():
-    data = request.form
-    username = data.get('username')
-    password = data.get('password')
-    role = data.get('role')
-    status = int(data.get('status'))
-    
-    controller = CreateUserController()
-    response = controller.createUser(username, password, role, status)
-    
-    return jsonify(response)
-
-@admin_bp.route('/display-users', methods=['GET'])
-@login_required
-def getAllUsers():
-    controller = GetAllUsersController()
-    users = controller.getAllUsers()
-    return jsonify(users)
 
 @admin_bp.route('/dashboard', methods=['GET'])
 @login_required
@@ -40,8 +17,3 @@ def user_accounts():
 @login_required
 def user_profiles():
     return render_template('UserAdminPage/user-profile.html')
-
-@admin_bp.route('/update-users', methods=['POST'])
-@login_required
-def updateUser():
-    pass
